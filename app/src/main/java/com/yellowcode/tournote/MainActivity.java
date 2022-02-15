dff3d068abf2208487215afd8a5daa55b1d7092e
TranNhat2512 <94840465+TranNhat2512@users.noreply.github.com>
2022-02-15 10:24:51 +0700

    Initial commit

---
+++ app/src/main/java/com/yellowcode/tournote/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yellowcode.tournote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TourNote_MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
